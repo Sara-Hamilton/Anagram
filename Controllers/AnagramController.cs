a5dbf16746888044de3c27e52780f57cdecb0ed3
dalia ramos and Sara Hamilton <sara@>
2018-02-13 08:52:32 -0800

    create skeleton

---
+++ Controllers/AnagramController.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Anagram.Models;
+
+namespace Anagram.Controllers
+{
+  public class AnagramController : Controller
+  {
+
+  }
+}
---
+++ Models/Anagram.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System;
+
+namespace Anagram.Models
+{
+  public class AnagramGenerator
+  {
+  
+  }
+}
