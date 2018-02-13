3739a3eece5391cb64b669c77314d59e043ee9a0
dalia ramos and Sara Hamilton <dalia@>
2018-02-13 13:08:20 -0800

    add Controller and Views

--- Controllers/AnagramController.cs
+++ Controllers/AnagramController.cs
@@ -7,6 +7,20 @@ namespace Anagram.Controllers
 {
   public class AnagramController : Controller
   {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
 
+    [HttpPost("/")]
+    public ActionResult Display()
+    {
+      AnagramGenerator newAnagramGenerator = new AnagramGenerator();
+      string wordToCompare = Request.Form["word-to-compare"];
+      List<string> wordList = new List<string>(){Request.Form["word-one"], Request.Form["word-two"], Request.Form["word-three"], Request.Form["word-four"], Request.Form["word-five"]};
+      List<string> matchingAnagrams = newAnagramGenerator.ReturnAnagramMatches(wordToCompare, wordList);
+      return View(matchingAnagrams);
+    }
   }
 }
--- Models/Anagram.cs
+++ Models/Anagram.cs
@@ -5,6 +5,40 @@ namespace Anagram.Models
 {
   public class AnagramGenerator
   {
-  
+    public bool IsAnagram(string wordOne, string wordTwo)
+    {
+      for (int i = 0; i < wordOne.Length; i++)
+      {
+        if (!wordTwo.Contains(wordOne[i].ToString()))
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    // public List<bool> AnagramList(string comparisonWord, List<string> wordList)
+    // {
+    //   List<bool> result = new List<bool>();
+    //   foreach (string word in wordList)
+    //   {
+    //     result.Add(IsAnagram(comparisonWord, word));
+    //   }
+    //   return result;
+    // }
+
+    public List<string> ReturnAnagramMatches(string comparisonWord, List<string> wordList)
+    {
+      List<string> result = new List<string>();
+      foreach (string word in wordList)
+      {
+        if (IsAnagram(comparisonWord, word))
+        {
+          result.Add(word);
+        }
+      }
+      return result;
+    }
+
   }
 }
